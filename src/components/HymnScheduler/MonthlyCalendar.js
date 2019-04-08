a44c709b43da67ec936403d6fab65629a004749a
ahnopologetic <ahnsv@bc.edu>
2019-04-08 23:14:47 +0900

    feat: libraries add

--- src/components/HymnScheduler/MonthlyCalendar.js
+++ src/components/HymnScheduler/MonthlyCalendar.js
@@ -1,11 +1,28 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { getMonth, getYear, getDate, getDaysInMonth } from 'date-fns'
+import { defaultProps } from 'recompose'
 
-export default class MonthlyCalendar extends Component {
+class MonthlyCalendar extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
+        const { selectedDay, selectedMonth, selectedYear } = this.props
         return (
             <div className="monthly-calendar">
-                monthly
+                {
+                    [...Array(getDaysInMonth(new Date(selectedYear, selectedMonth, selectedDay))).keys()].map(i => i + 1).map((day, key) => <div key={key} className={`monthly-calendar--day${key}`}>{day}</div>)
+                }
             </div>
         )
     }
 }
+
+const withDefaultProps = defaultProps({
+    selectedYear: getYear(new Date()),
+    selectedMonth: getMonth(new Date()),
+    selectedDay: getDate(new Date())
+})
+
+export default withDefaultProps(MonthlyCalendar)
