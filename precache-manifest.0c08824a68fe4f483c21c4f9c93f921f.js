df3173e53638ec99fe97ad7e7f1a8bf3fc85d720
Humphrey Ahn <ahnsv@bc.edu>
2019-05-18 20:29:49 +0900

    Updates

---
+++ precache-manifest.0c08824a68fe4f483c21c4f9c93f921f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bff2af52870c255fb13ccc1e366bee5",
+    "url": "/hymn-project/static/media/star.1bff2af5.svg"
+  },
+  {
+    "revision": "4d7dedfd002986d4a551",
+    "url": "/hymn-project/static/css/main.a65388b0.chunk.css"
+  },
+  {
+    "revision": "d801c7fab33ea04f579c",
+    "url": "/hymn-project/static/js/runtime~main.e0274173.js"
+  },
+  {
+    "revision": "46ea391cdf79d16cb56a",
+    "url": "/hymn-project/static/js/2.8663c674.chunk.js"
+  },
+  {
+    "revision": "d2029f73c869adb09df9e460effee306",
+    "url": "/hymn-project/static/media/hamburger.d2029f73.svg"
+  },
+  {
+    "revision": "17fe5d4d91bb436295e85d12493e51b9",
+    "url": "/hymn-project/static/media/calendar.17fe5d4d.svg"
+  },
+  {
+    "revision": "4d7dedfd002986d4a551",
+    "url": "/hymn-project/static/js/main.175251c5.chunk.js"
+  },
+  {
+    "revision": "087dd08bf5e50a6e6a7603809afa909d",
+    "url": "/hymn-project/static/media/move.087dd08b.svg"
+  },
+  {
+    "revision": "ec97192c2c45a576352d08537753d11c",
+    "url": "/hymn-project/static/media/gauge.ec97192c.svg"
+  },
+  {
+    "revision": "37e7df584b7dc5b6f329918674449f70",
+    "url": "/hymn-project/static/media/left.37e7df58.svg"
+  },
+  {
+    "revision": "57c862dbca60e53645acc47f943f4504",
+    "url": "/hymn-project/static/media/right.57c862db.svg"
+  },
+  {
+    "revision": "4e3427d3111cd3a2f7e49a95788d09e2",
+    "url": "/hymn-project/index.html"
+  }
+];
